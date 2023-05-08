3f01d6554e49455312fdba066c5486cf0aea2a39
samirdourado <samir3500@gmail.com>
2023-05-08 20:30:51 -0300

    fix: images not appear, testing deploy

--- src/database.ts
+++ src/database.ts
@@ -2,42 +2,42 @@ const data = [
 
     {
         id: 1,
-        image: '../src/assets/covers/hamburgueriav2.png',
+        image: './src/assets/covers/hamburgueriav2.png',
         title: 'Hamburgueria V2',
         description: 'Sistema de compras para usuario com login e senha.',
         techs: [
-            '../src/assets/techs/react.svg', 
-            '../src/assets/techs/typescript.svg', 
-            '../src/assets/techs/javascript.svg', 
-            '../src/assets/techs/git-icon.svg'
+            './src/assets/techs/react.svg', 
+            './src/assets/techs/typescript.svg', 
+            './src/assets/techs/javascript.svg', 
+            './src/assets/techs/git-icon.svg'
         ],
         url: 'https://hamburgueria-v2-samirdourado.vercel.app/'
     },
 
     {
         id: 2,
-        image: '../src/assets/covers/kenziehub.png',
+        image: './src/assets/covers/kenziehub.png',
         title: 'Kenzie Hub',
         description: 'Cadastro de technologias nos moldes de todoList.',
         techs: [
-            '../src/assets/techs/react.svg', 
-            '../src/assets/techs/typescript.svg', 
-            '../src/assets/techs/javascript.svg', 
-            '../src/assets/techs/git-icon.svg'
+            './src/assets/techs/react.svg', 
+            './src/assets/techs/typescript.svg', 
+            './src/assets/techs/javascript.svg', 
+            './src/assets/techs/git-icon.svg'
         ],
         url: 'https://react-entrega-kenzie-hub-samirdourado.vercel.app/'
     },
 
     {
         id: 3,
-        image: '../src/assets/covers/hamburgueriav1.png',
+        image: './src/assets/covers/hamburgueriav1.png',
         title: 'Hamburgueria V1',
         description: 'Carrinho de compras dinâmico.',
         techs: [
-            '../src/assets/techs/react.svg', 
-            '../src/assets/techs/typescript.svg', 
-            '../src/assets/techs/javascript.svg', 
-            '../src/assets/techs/git-icon.svg'
+            './src/assets/techs/react.svg', 
+            './src/assets/techs/typescript.svg', 
+            './src/assets/techs/javascript.svg', 
+            './src/assets/techs/git-icon.svg'
         ],
         url: 'https://hamburgueria-da-kenzie-samirdourado.vercel.app/'        
     },
