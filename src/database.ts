f822a5e4cf484d5687a36c73b40ac6dbe4f98208
samirdourado <samir3500@gmail.com>
2023-05-08 20:41:25 -0300

    Fix: Database

--- src/database.ts
+++ src/database.ts
@@ -2,56 +2,56 @@ const data = [
 
     {
         id: 1,
-        image: './src/assets/covers/hamburgueriav2.png',
+        image: './assets/covers/hamburgueriav2.png',
         title: 'Hamburgueria V2',
         description: 'Sistema de compras para usuario com login e senha.',
         techs: [
-            './src/assets/techs/react.svg', 
-            './src/assets/techs/typescript.svg', 
-            './src/assets/techs/javascript.svg', 
-            './src/assets/techs/git-icon.svg'
+            './assets/techs/react.svg', 
+            './assets/techs/typescript.svg', 
+            './assets/techs/javascript.svg', 
+            './assets/techs/git-icon.svg'
         ],
         url: 'https://hamburgueria-v2-samirdourado.vercel.app/'
     },
 
     {
         id: 2,
-        image: './src/assets/covers/kenziehub.png',
+        image: './assets/covers/kenziehub.png',
         title: 'Kenzie Hub',
         description: 'Cadastro de technologias nos moldes de todoList.',
         techs: [
-            './src/assets/techs/react.svg', 
-            './src/assets/techs/typescript.svg', 
-            './src/assets/techs/javascript.svg', 
-            './src/assets/techs/git-icon.svg'
+            './assets/techs/react.svg', 
+            './assets/techs/typescript.svg', 
+            './assets/techs/javascript.svg', 
+            './assets/techs/git-icon.svg'
         ],
         url: 'https://react-entrega-kenzie-hub-samirdourado.vercel.app/'
     },
 
     {
         id: 3,
-        image: './src/assets/covers/hamburgueriav1.png',
+        image: './assets/covers/hamburgueriav1.png',
         title: 'Hamburgueria V1',
         description: 'Carrinho de compras dinâmico.',
         techs: [
-            './src/assets/techs/react.svg', 
-            './src/assets/techs/typescript.svg', 
-            './src/assets/techs/javascript.svg', 
-            './src/assets/techs/git-icon.svg'
+            './assets/techs/react.svg', 
+            './assets/techs/typescript.svg', 
+            './assets/techs/javascript.svg', 
+            './assets/techs/git-icon.svg'
         ],
         url: 'https://hamburgueria-da-kenzie-samirdourado.vercel.app/'        
     },
 
     {
         id: 4,
-        image: '../src/assets/covers/nukenzie.png',
+        image: './assets/covers/nukenzie.png',
         title: 'Nu Kenzie',
         description: 'Sistem de entrada e saída de valores.',
         techs: [
-            '../src/assets/techs/react.svg', 
-            '../src/assets/techs/typescript.svg', 
-            '../src/assets/techs/javascript.svg', 
-            '../src/assets/techs/git-icon.svg'
+            './assets/techs/react.svg', 
+            './assets/techs/typescript.svg', 
+            './assets/techs/javascript.svg', 
+            './assets/techs/git-icon.svg'
         ],
         url: 'https://nu-kenzie-samirdourado.vercel.app/'        
     }
